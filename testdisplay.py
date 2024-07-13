7861250621d3406002ab4ea9b15eb3472ea9dba9
Don Girman <dgirman@girman.com>
2024-07-13 12:35:18 -0700

    init setup 2

---
+++ testdisplay.py
@@ -0,0 +1,15 @@
+from PyQt5 import QtWidgets
+from gui.power_bar import PowerBar
+
+
+app = QtWidgets.QApplication([])
+
+
+PowerBar(10)
+PowerBar(3)
+PowerBar(["#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b", "#fdae61", "#f46d43", "#d53e4f", "#9e0142"])
+PowerBar(["#a63603", "#e6550d", "#fd8d3c", "#fdae6b", "#fdd0a2", "#feedde"])
+
+volume = PowerBar(["#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b", "#fdae61", "#f46d43", "#d53e4f", "#9e0142"])
+volume.show()
+app.exec_()
